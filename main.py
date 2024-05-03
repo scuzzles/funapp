84241ac5977a186af9a8692b4214686d157ebeaa
scuzzles <68835586+scuzzles@users.noreply.github.com>
2024-05-03 00:42:30 -0500

    made better
    removed unnecessary files and code

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello from vercel app"
+    return "hello from render app"
 
-@app.route("/about")
-def about():
-    return "hello about"
 
 app.run(host="0.0.0.0", port=8080)
